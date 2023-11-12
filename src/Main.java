734d415160319ca29164f9a499d905cb0c3c05bb
Dylan Higgins <higgindy@mail.uc.edu>
2023-11-12 13:10:39 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your birth month (1-12): ");
+        if (scanner.hasNextInt())
+        {
+            int birthMonth = scanner.nextInt();
+            if (birthMonth >= 1 && birthMonth <= 12)
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+            else
+            {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        }
+
+    }
+}
